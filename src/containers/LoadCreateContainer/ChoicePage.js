53d05048233d780f4a02ffed3db10fa988ca8b84
RatBoyJim <>
2021-02-14 14:38:14 +0000

    auth user info showing on load/create page

--- src/containers/LoadCreateContainer/ChoicePage.js
+++ src/containers/LoadCreateContainer/ChoicePage.js
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect, Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const ChoicePage = ({unsetSelectedCharacter, userDataLoaded}) => {
+
+  const { user } = useAuth0();
+  const { name, picture, email } = user;
 
   useEffect(() => {
     unsetSelectedCharacter();
@@ -31,7 +35,26 @@ const ChoicePage = ({unsetSelectedCharacter, userDataLoaded}) => {
     <h3 className="load_text">Load</h3>
     </Link>
     </div>
-    
+    <div>
+      <div className="row align-items-center profile-header">
+        <div className="col-md-2 mb-3">
+          <img
+            src={picture}
+            alt="Profile"
+            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+          />
+        </div>
+        <div className="col-md text-center text-md-left">
+          <h2>{name}</h2>
+          <p className="lead text-muted">{email}</p>
+        </div>
+      </div>
+      <div className="row">
+        <pre className="col-12 text-light bg-dark p-4">
+          {JSON.stringify(user, null, 2)}
+        </pre>
+      </div>
+    </div>
   </div>
   )};
 

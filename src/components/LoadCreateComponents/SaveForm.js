15ca69df6513178a5fedf671f7ac6c67b3f0de76
RatBoyJim <>
2021-02-16 12:09:16 +0000

    working with generated tokens

--- src/components/LoadCreateComponents/SaveForm.js
+++ src/components/LoadCreateComponents/SaveForm.js
@@ -8,6 +8,8 @@ const SaveForm = ({logInNewUser}) => {
     const [newPassword, setNewPassword] = useState("");
     const { user } = useAuth0();
     const { name, picture, email } = user;
+    const { getAccessTokenSilently } = useAuth0();
+    
 
     const handleUsernameChange = (evt) => {
         setNewUsername(evt.target.value);
@@ -21,42 +23,46 @@ const SaveForm = ({logInNewUser}) => {
         evt.preventDefault();
         const usernameToSubmit = newUsername.trim();
         const passwordToSubmit = newPassword.trim();
-        saveNewUser(usernameToSubmit, passwordToSubmit); 
+        callApi(usernameToSubmit, passwordToSubmit); 
         
         
         // setNewUsername("");
         // setNewPassword("");
     }
 
-    const saveNewUser = (usernameToSubmit, passwordToSubmit, email) => {
-        // Simple POST request with a JSON body using fetch
+
+    const callApi = async (usernameToSubmit, passwordToSubmit) => {
+
+        const token = await getAccessTokenSilently();
+
         const requestOptions = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImVkSldza3pSanB0dzNZQmp0OXhJdSJ9.eyJpc3MiOiJodHRwczovL2Rldi1wd3o0b2tibi5ldS5hdXRoMC5jb20vIiwic3ViIjoiaDRRVVJiYVFBRjEwZ21tQXdYRTZmamUzTjRaVGNoa2lAY2xpZW50cyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9hcGkiLCJpYXQiOjE2MTMzOTUyNDQsImV4cCI6MTYxMzQ4MTY0NCwiYXpwIjoiaDRRVVJiYVFBRjEwZ21tQXdYRTZmamUzTjRaVGNoa2kiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.BWKd9wTPl0DjW3iBKeObjiPuH7ZjREhxg0nxMSRJcyNHQaKLz8d8vWBw_9h_KmZil4g9XLs1Jf9FFonv7Wuy0EyAsWJKHEpJvw1qZ7JSmgV5TQCeuxRgExNOIByKEv0Ew45sq-K4wE35fbUKa5YlVh9_EUM0mtB_iQ4Gz8E-d1yW1tOoUeu8rQITDuNqEuuw2aRSTZq-Wr4-qsaOxQSR7uA0ThFmJRABCh7_WWSVKS59jzyrzbbikeZrL3ehL47Oy_ONtcoBVAx3D4N35DGD1g5bIPx5H3TdtwgUUyApcrI_DSg0CWgn2XyKcTyqZwgr26BNqWUcCXtTeXLxilVN5w'
+                authorization: `Bearer ${token}`
             },
             data: {
                 grant_type: 'client_credentials',
                 client_id: 'h4QURbaQAF10gmmAwXE6fje3N4ZTchki',
                 client_secret: 'bpXbKh0yAu5BD1UvcSZLxdmBsy8oa3y_dEE_w3X3aZEEwkDq6CH6-4sLmvDHAxV0',
                 audience: 'http://localhost:8080/api'
-              },
+            },
             body: JSON.stringify({ 
-              userName: usernameToSubmit,
-              password: passwordToSubmit,
-              imageURL: "fakeImgUrl",
-              animals: []
-            })
-        };
-        return fetch('http://localhost:8080/api/users', requestOptions)
-            .then(() => logInNewUser({
-                username: usernameToSubmit,
-                password: passwordToSubmit
-            }))
-            .then(setNewUsername(""))
-            .then(setNewPassword(""))
-      }
+                userName: usernameToSubmit,
+                password: passwordToSubmit,
+                imageURL: "fakeImgUrl",
+                animals: []
+                })
+            };
+            return await fetch('http://localhost:8080/api/users', requestOptions)
+                .then(() => logInNewUser({
+                    username: usernameToSubmit,
+                    password: passwordToSubmit
+                }))
+                .then(setNewUsername(""))
+                .then(setNewPassword(""))
+        }
+    
 
 
     return(
--- src/containers/LoadCreateContainer/CreatePage.js
+++ src/containers/LoadCreateContainer/CreatePage.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect, Link } from "react-router-dom";
-
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 const CreatePage = ({allAnimals, userData, setHasSelectedCharacter, getUserData, setLoaded, userDataLoaded, getAllAnimalData}) => {
 
-  
+  const { getAccessTokenSilently } = useAuth0();
 
   const [formData, setFormData] = useState({
     name: '',
@@ -46,14 +46,16 @@ const CreatePage = ({allAnimals, userData, setHasSelectedCharacter, getUserData,
 
 
 
-const saveNewAnimal = (data) => {
-  // Simple POST request with a JSON body using fetch
+const saveNewAnimal = async (data) => {
+  
+  const token = await getAccessTokenSilently();
+
   const requestOptions = {
       
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImVkSldza3pSanB0dzNZQmp0OXhJdSJ9.eyJpc3MiOiJodHRwczovL2Rldi1wd3o0b2tibi5ldS5hdXRoMC5jb20vIiwic3ViIjoiaDRRVVJiYVFBRjEwZ21tQXdYRTZmamUzTjRaVGNoa2lAY2xpZW50cyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9hcGkiLCJpYXQiOjE2MTMzOTUyNDQsImV4cCI6MTYxMzQ4MTY0NCwiYXpwIjoiaDRRVVJiYVFBRjEwZ21tQXdYRTZmamUzTjRaVGNoa2kiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.BWKd9wTPl0DjW3iBKeObjiPuH7ZjREhxg0nxMSRJcyNHQaKLz8d8vWBw_9h_KmZil4g9XLs1Jf9FFonv7Wuy0EyAsWJKHEpJvw1qZ7JSmgV5TQCeuxRgExNOIByKEv0Ew45sq-K4wE35fbUKa5YlVh9_EUM0mtB_iQ4Gz8E-d1yW1tOoUeu8rQITDuNqEuuw2aRSTZq-Wr4-qsaOxQSR7uA0ThFmJRABCh7_WWSVKS59jzyrzbbikeZrL3ehL47Oy_ONtcoBVAx3D4N35DGD1g5bIPx5H3TdtwgUUyApcrI_DSg0CWgn2XyKcTyqZwgr26BNqWUcCXtTeXLxilVN5w'
+        authorization: `Bearer ${token}`
     },
     data: {
         grant_type: 'client_credentials',
@@ -76,7 +78,7 @@ const saveNewAnimal = (data) => {
           imageURL: userData[0].imageURL}
       })
   };
-  return fetch('http://localhost:8080/api/animals', requestOptions)
+  return await fetch('http://localhost:8080/api/animals', requestOptions)
   .then(setHasSelectedCharacter(true))
   .then(() => setLoaded(true))
   .then(getUserData())
